dafc61e1618ba4cfb604bc773610e53883deb7d5
superbabii <nzrbabii@gmail.com>
2024-10-15 03:29:58 +0300

    f

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ for question_id, data in random_questions:
     correct_answer = data["answer"]
 
     # Get model's prediction
-    answer, snippets, scores = medrag.answer(question=question, options=options, k=1)
+    answer, snippets, scores = medrag.answer(question=question, options=options, k=3)
   
     # Parse the generated answer and compare with correct answer
     try:
